4b3bd7af716a9bfd15658636dc6dd29de1bc0912
Monica Bustamante <lilianalambda@gmail.com>
2020-09-10 12:50:20 -0700

    Graphs

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -2,7 +2,6 @@ from room import Room
 from player import Player
 from world import World
 
-import queue
 import random
 from ast import literal_eval
 
@@ -18,26 +17,54 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph = literal_eval(open(map_file, "r").read())
+room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
-print(room_graph)
 
-# Print an ASCII map
+#Print an ASCII map
 world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-traversal_graph = {}
-move_reverse = {'n': 's', 
-                's': 'n', 
-                'e': 'w', 
-                'w': 'e'}
-         
-    
+visited = set()
+move_back = {'n': 's', 
+           's': 'n', 
+           'w': 'e', 
+           'e':'w'}
 
-# TRAVERSAL TEST
+def traverse(room, visited=None):
+    path = []
+    room = player.current_room
+    if visited is None:                              #store visited nodes/rooms
+        visited = set()
+    for sequence in room.get_exits():                #current room to see possible exit
+        player.travel(sequence)
+        room = player.current_room
+        if room in visited:                          #room visited, backtrack
+            player.travel(move_back[sequence])
+        else:                                        #not visited before, add room visited 
+            visited.add(room)
+            path.append(sequence)
+            path = path + traverse(room, visited)    #append and path
+            player.travel(move_back[sequence])
+            path.append(move_back[sequence])
+
+    return path
+    print(path)
+
+def graph(visited, move_back, node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbour in move_back[node]:
+            graph(visited, move_back, neighbour)
+
+# Driver Code
+graph(visited, move_back, 'n')
+traversal_path = traverse(player.current_room)
+
+# TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
@@ -51,8 +78,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
 
 #######
 # UNCOMMENT TO WALK AROUND
